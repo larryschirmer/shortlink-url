8fa814189b78c30c54eaa3f5582667be021d6ed6
Larry Schirmer <larryschirmer@gmail.com>
2021-07-16 20:54:42 -0400

    feat: initialize state with reducer's inital state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Head from "next/head";
 
-import UrlList from "../Components/UrlList";
+import UrlList from "@components/UrlList";
+
+import { Provider } from "@context/index";
+import reducer, { initialState } from "@context/urls";
 
 const Home = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div>
+    <Provider value={{ state, dispatch }}>
       <Head>
         <title>URL Shortened List</title>
       </Head>
       <UrlList />
-    </div>
+    </Provider>
   );
 };
 
