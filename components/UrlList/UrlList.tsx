a1ef382510fe52eb534daadcc95617e551bcc987
Larry Schirmer <larryschirmer@gmail.com>
2021-07-16 21:51:34 -0400

    feat: display content from state

--- components/UrlList/UrlList.tsx
+++ components/UrlList/UrlList.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 
+import useStateContext, { Context } from "@context/index";
+import { State, Actions } from "@context/urls/types";
+
 import styles from "./UrlList.module.scss";
 
 const { "url-list": urlListClass } = styles;
 
 const UrlList = () => {
+  const { state } = useStateContext<Context<State, Actions>>();
+
   return (
     <div className={urlListClass}>
-      <div>Card One</div>
-      <div>Card Two</div>
+      {state.data?.map(({ _id, name, slug, url, opens, isListed }) => {
+        const opensAmt = opens.length;
+        return (
+          <div key={_id}>
+            <div>
+              {name} - [listed: {isListed.toString()}]
+            </div>
+            <div>
+              <div>
+                <div>domain.com/{slug}</div>
+                <div>{url}</div>
+              </div>
+              <div>
+                <div>{opensAmt} Opens</div>
+                <div>{opens[opensAmt - 1]}</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
