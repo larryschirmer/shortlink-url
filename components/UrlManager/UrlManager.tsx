fe40a0b5718da843ea41d694cd0d52c569cc882d
Larry Schirmer <larryschirmer@gmil.com>
2021-11-05 15:33:59 +0000

    feat: fetch links on demand (tmp placement)

--- components/UrlManager/UrlManager.tsx
+++ components/UrlManager/UrlManager.tsx
@@ -1,18 +1,27 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/pro-regular-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/pro-regular-svg-icons';
 
-import UrlList from "@components/UrlList";
+import UrlList from '@components/UrlList';
 
-import styles from "./UrlManager.module.scss";
+import useStateContext from '@context/index';
+import { getLinks } from '@context/urls/operations';
 
-const { "url-manager": urlManagerClass } = styles;
+import styles from './UrlManager.module.scss';
+
+const { 'url-manager': urlManagerClass } = styles;
 
 const UrlManager = () => {
+  const { dispatch } = useStateContext();
+
+  const handleFetchLinks = () => {
+    dispatch(getLinks());
+  };
+
   return (
     <div className={urlManagerClass}>
-      <button>
-        New{" "}
+      <button onClick={handleFetchLinks}>
+        New{' '}
         <span>
           <FontAwesomeIcon icon={faLink} />
         </span>
