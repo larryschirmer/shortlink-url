c3fc050bbae0e026b4c65af176a8036f4bfb9d18
Larry Schirmer <larryschirmer@gmil.com>
2021-11-05 22:21:29 +0000

    chore: manage fetch links in different component

--- components/UrlManager/UrlManager.tsx
+++ components/UrlManager/UrlManager.tsx
@@ -4,23 +4,14 @@ import { faLink } from '@fortawesome/pro-regular-svg-icons';
 
 import UrlList from '@components/UrlList';
 
-import useStateContext from '@context/index';
-import { getLinks } from '@context/urls/operations';
-
 import styles from './UrlManager.module.scss';
 
 const { 'url-manager': urlManagerClass } = styles;
 
 const UrlManager = () => {
-  const { dispatch } = useStateContext();
-
-  const handleFetchLinks = () => {
-    dispatch(getLinks());
-  };
-
   return (
     <div className={urlManagerClass}>
-      <button onClick={handleFetchLinks}>
+      <button>
         New{' '}
         <span>
           <FontAwesomeIcon icon={faLink} />
