5104a13cb42a4ac7fcc44515c9b46a7ad41f77ee
Larry Schirmer <larryschirmer@gmail.com>
2021-11-06 21:49:05 +0000

    feat: add app management buttons

--- components/UrlManager/UrlManager.tsx
+++ components/UrlManager/UrlManager.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLink } from '@fortawesome/pro-regular-svg-icons';
+import { faFilePlus, faTrash, faSignInAlt } from '@fortawesome/pro-regular-svg-icons';
 
 import UrlList from '@components/UrlList';
 import Menu from '@components/Menu';
 
 import styles from './UrlManager.module.scss';
+import Button from '@components/Button';
 
-const { 'url-manager': urlManagerClass } = styles;
+const {
+  'url-manager': urlManagerClass,
+  header: headerClass,
+  'link-management': linkMangementClass,
+} = styles;
 
 const UrlManager = () => {
   return (
     <div className={urlManagerClass}>
-      <UrlList />
-      <Menu />
+      <div className={headerClass}>
+        <div>
+          <h1>URL Shortener - larryschirmer</h1>
+        </div>
+        <div>
+          <Button>
+            <FontAwesomeIcon icon={faFilePlus} />
+          </Button>
+          <Button>
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+          <Button>
+            <FontAwesomeIcon icon={faSignInAlt} />
+          </Button>
+        </div>
+      </div>
+      <div className={linkMangementClass}>
+        <UrlList />
+        <Menu />
+      </div>
     </div>
   );
 };
