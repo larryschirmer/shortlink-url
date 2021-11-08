f13a95574f751cc0b5bfed94dfcd809a175418ca
Larry Schirmer <larryschirmer@gmail.com>
2021-11-08 14:52:24 +0000

    feat: detect existing cookie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
-import React, { useReducer } from "react";
-import Head from "next/head";
+import React, { useReducer } from 'react';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 
-import PageHeader from "@components/PageHeader";
-import UrlManager from "@components/UrlManager";
+import PageHeader from '@components/PageHeader';
+import UrlManager from '@components/UrlManager';
 
-import { Provider, reducer, initialState } from "@context/index";
+import { Provider, reducer, initialState } from '@context/index';
 
-const Home = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+type Props = { cookies: { [cookie: string]: string } };
+
+const Home = ({ cookies }: Props) => {
+  const [state, dispatch] = useReducer(reducer, {
+    ...initialState,
+    isLoggedIn: !!cookies['charming-smile'],
+  });
+
   return (
     <Provider value={{ state, dispatch }}>
       <Head>
@@ -17,6 +24,14 @@ const Home = () => {
       <UrlManager />
     </Provider>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const {
+    req: { cookies },
+  } = ctx;
+
+  return { props: { cookies } };
 };
 
 export default Home;
