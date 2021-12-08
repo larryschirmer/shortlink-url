08feca6dd552c4032492c17807e3820eb4db84c0
Larry Schirmer <larryschirmer@gmail.com>
2021-12-08 23:24:09 +0000

    chore: import jest form accessibility

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,20 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '@context/(.*)': '<rootDir>/context/$1',
+    '@components/(.*)': '<rootDir>/components/$1',
+    '@pages/(.*)': '<rootDir>/pages/$1',
+    '@styles/(.*)': '<rootDir>/styles/$1',
+    '@utils/(.*)': '<rootDir>/utils/$1',
+    'd3-scale': '<rootDir>/node_modules/d3-scale/dist/d3-scale.min.js',
+    'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
+    'd3-interpolate':
+      '<rootDir>/node_modules/d3-interpolate/dist/d3-interpolate.min.js',
+    'd3-format': '<rootDir>/node_modules/d3-format/dist/d3-format.min.js',
+    'd3-scale': '<rootDir>/node_modules/d3-scale/dist/d3-scale.min.js',
+    'd3-time': '<rootDir>/node_modules/d3-time/dist/d3-time.min.js',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
