35944cb7bb79f5eb1024df5b7698c6aeaf374f75
Larry Schirmer <larryschirmer@gmail.com>
2021-12-08 23:21:52 +0000

    feat: support button aria attrs

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -8,18 +8,30 @@ const { button: buttonClass, secondary: secondaryClass } = styles;
 export type Props = {
   children: ReactNode;
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  'aria-label'?: string;
   disabled?: boolean;
   isSecondary?: boolean;
   onClick?: () => void;
 };
 
 const Button = (props: Props) => {
-  const { children, isSecondary = false, disabled = false, type = 'button', onClick } = props;
+  const {
+    children,
+    type = 'button',
+    'aria-label': ariaLabel = 'button',
+    disabled = false,
+    isSecondary = false,
+    onClick,
+  } = props;
   const buttonClasses = classNames(buttonClass, {
     [secondaryClass]: isSecondary,
   });
   return (
-    <button {...{ type, disabled, onClick }} className={buttonClasses}>
+    <button
+      {...{ type, disabled, onClick }}
+      className={buttonClasses}
+      aria-label={ariaLabel}
+    >
       {children}
     </button>
   );
