7f876e9dcf2bc4a7776bb4eabf539b93cfcbb54d
Larry Schirmer <larryschirmer@gmail.com>
2021-12-20 23:58:53 +0000

    fix: missing key warning

--- components/AccordianList/AccordianList.test.tsx
+++ components/AccordianList/AccordianList.test.tsx
@@ -3,14 +3,14 @@
  */
 
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
 
 import AccordianList from './AccordianList';
 
 test('renders title', async () => {
   const title = 'List Title';
-  const list = [<></>, <></>];
+  const list = [<p key='one'>One</p>, <p key='two'>Two</p>];
   render(<AccordianList {...{ title, list }} />);
 
   expect(screen.getByText(title)).toBeInTheDocument();
@@ -18,7 +18,7 @@ test('renders title', async () => {
 
 test('callbacks are triggered on click', async () => {
   const title = 'List Title';
-  const list = [<></>, <></>];
+  const list = [<p key='one'>One</p>, <p key='two'>Two</p>];
   const handleHeaderOpen = jest.fn();
   const handleHeaderClose = jest.fn();
   render(
