1a0e852adad3708dd4d4ccb2053286327b409226
Larry Schirmer <larryschirmer@gmail.com>
2022-01-05 16:23:02 +0000

    fix: keep edit mode open after change

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import classNames from 'classnames';
-import { observer } from 'mobx-react-lite';
 
 import EditForm from '@components/EditForm';
 
-import { useMst } from '@models/index';
-
 import styles from './Menu.module.scss';
 
-const { menu: menuClass, hidden: hiddenClass } = styles;
+const { menu: menuClass } = styles;
 
 const Menu = () => {
-  const {
-    app: { selectedLink, createMode },
-  } = useMst();
-
-  const menuClasses = classNames(menuClass, {
-    [hiddenClass]: !selectedLink && !createMode,
-  });
-
   return (
-    <div className={menuClasses}>
+    <div className={menuClass}>
       <EditForm />
     </div>
   );
 };
 
-export default observer(Menu);
+export default Menu;
--- components/UrlManager/UrlManager.tsx
+++ components/UrlManager/UrlManager.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 
 import UrlList from '@components/UrlList';
 import Menu from '@components/Menu';
+
+import { useMst } from '@models/index';
 
 import styles from './UrlManager.module.scss';
 
 const { 'url-manager': urlManagerClass } = styles;
 
 const UrlManager = () => {
+  const {
+    app: { selectedLink, createMode },
+  } = useMst();
+
   return (
     <div className={urlManagerClass}>
       <UrlList />
-      <Menu />
+      {(!!selectedLink || createMode) && <Menu />}
     </div>
   );
 };
 
-export default UrlManager;
+export default observer(UrlManager);
